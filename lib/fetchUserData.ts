522e175cdbc826c31a72d6a22c00cca7cadefdb9
Jamlons <joel.willoughby29@outlook.com>
2025-03-11 15:10:24 +0800

    test

--- lib/fetchUserData.ts
+++ lib/fetchUserData.ts
@@ -1,10 +1,12 @@
 import { endpoint } from '@/utils/endpoint'
 
 export async function FetchUserData(searchQuery: string) {
+
   console.log(`${endpoint}/api/fetchUserData?query=${searchQuery}`)
   const response = await fetch(`${endpoint}/api/fetchUserData?query=${searchQuery}`);
+
   if (!response.ok) {
-    throw new Error('Failed to fetch user data');
+    throw new Error(`${endpoint}/api/fetchUserData?query=${searchQuery}`);
   }
   return response.json();
 }
