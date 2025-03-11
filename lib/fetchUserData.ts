e86cade38989bf03b935582d9a4e0c0487318165
Jamlons <joel.willoughby29@outlook.com>
2025-03-11 14:02:17 +0800

    test 2.0

--- lib/fetchUserData.ts
+++ lib/fetchUserData.ts
@@ -1,8 +1,14 @@
 export async function FetchUserData(searchQuery: string) {
-    const response = await fetch(`/api/fetchUserData?query=${searchQuery}`);
-    if (!response.ok) {
-      throw new Error('Failed to fetch user data');
-    }
-    return response.json();
+  const baseUrl = typeof window !== "undefined" ? window.location.origin : 'http://localhost:3000'; // Or the appropriate server URL
+  
+  // Create the full URL by concatenating the base URL and the relative API path
+  const url = `${baseUrl}/api/fetchUserData?query=${searchQuery}`;
+
+  const response = await fetch(url);
+
+  if (!response.ok) {
+    throw new Error('Failed to fetch user data');
+  }
+  return response.json();
 }
   
